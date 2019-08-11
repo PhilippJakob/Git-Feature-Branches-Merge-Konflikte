4c34369924d2ea4ebfb90cbd2a21431cb5d53b2b
Philipp Jakob <jakob@osztiem.de>
2019-08-11 19:14:42 +0200

    Hr.Jakob legt Java Klasse an

---
+++ src/gitFeatureBranches/HrJakobKonflikt.java
@@ -0,0 +1,14 @@
+package gitFeatureBranches;
+
+public class HrJakobKonflikt
+{
+   public void schwimmen()
+   {
+	  boolean lVergleich = true;
+	  while(lVergleich == true)
+	  {
+		 System.out.println(lVergleich);
+	  };
+   }
+   
+}
