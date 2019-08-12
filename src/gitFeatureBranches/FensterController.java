3ba6f969c29eb80f75ce10149c91b642b80b0929
rudek.heinrich <rudek.heinrich@PC230118.osz.intern>
2019-08-12 11:14:05 +0200

    hier neue version arbeitet mal ;D

---
+++ src/gitFeatureBranches/FensterController.java
@@ -0,0 +1,43 @@
+package gitFeatureBranches;
+
+
+   import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+   import javafx.scene.control.ChoiceBox;
+   import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.control.Tooltip;
+
+   public class FensterController {
+
+       @FXML
+       private ChoiceBox<String> Personauswahl;
+
+       @FXML
+       private Label Lrang4;
+
+       @FXML
+       private Label Lrang5;
+
+       @FXML
+       private Label Lrang1;
+
+       @FXML
+       private Label Lrang2;
+
+       @FXML
+       private Label Lrang3;
+       
+       @FXML
+       public void initialize()
+       {
+    	 
+    	  Personauswahl.getItems().addAll("HerrJacob", "KeyCode32", "Patrik");
+    	  Personauswahl.setTooltip(new Tooltip("Wähle die Person aus"));
+       }
+       
+
+   }
+
+
