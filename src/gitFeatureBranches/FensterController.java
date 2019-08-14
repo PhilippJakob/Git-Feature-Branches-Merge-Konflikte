315de6a371a4988d812be70b6acb7c7f521ccc3a
piel.florian_martin <piel.florian_martin@PC230120.osz.intern>
2019-08-14 12:06:37 +0200

    DatenBank zugang hinzugefügt

--- src/gitFeatureBranches/FensterController.java
+++ src/gitFeatureBranches/FensterController.java
@@ -1,16 +1,19 @@
 package gitFeatureBranches;
 
 
-   import javafx.collections.FXCollections;
+
+
+
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
    import javafx.scene.control.ChoiceBox;
    import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.control.Tooltip;
-
+import javafx.scene.control.MenuBar;
    public class FensterController {
-
+	  private static DBVerbindung dbVerbindung = new DBVerbindung();
        @FXML
        private ChoiceBox<String> Personauswahl;
 
@@ -32,8 +35,12 @@ import javafx.scene.control.Tooltip;
        @FXML
        public void initialize()
        {
-    	 
+    	  if (dbVerbindung.verbinden("dbserver", "dbpr_termin", "dblkuser", "lkbenutzer")== false)
+    	  {
+    		return;
+    	  }
     	  Personauswahl.getItems().addAll("HerrJacob", "KeyCode32", "Patrik");
+
     	  Personauswahl.setTooltip(new Tooltip("Wähle die Person aus"));
        }
        
