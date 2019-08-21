7e904d90b1105a4f19b38d2bcdefcbe248d0fc0d
madrian.patrick <madrian.patrick@PC230101.osz.intern>
2019-08-21 14:43:20 +0200

    ...

--- src/gitFeatureBranches/GruppenbearbeitungController.java
+++ src/gitFeatureBranches/GruppenbearbeitungController.java
@@ -2,20 +2,24 @@ package gitFeatureBranches;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
    public class GruppenbearbeitungController {
 
        @FXML
        private Button btErstellen;
-
+       
        @FXML
-       private ChoiceBox<?> cbGruppe;
+       private TextField tfGruppe;
 
        @FXML
        private Button btLöschen;
 
        @FXML
        private ChoiceBox<?> cbPerson;
+       
+       
+       
 
    }
 
--- src/gitFeatureBranches/GruppenzuordnungController.java
+++ src/gitFeatureBranches/GruppenzuordnungController.java
@@ -1,6 +1,22 @@
 package gitFeatureBranches;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 
-public class GruppenzuordnungController
-{
+public class GruppenzuordnungController {
 
+    @FXML
+    private Button btEntziehen;
+
+    @FXML
+    private ChoiceBox<?> cbGruppe;
+
+    @FXML
+    private ChoiceBox<?> cbPerson;
+
+    @FXML
+    private Button btZuweisen;
+
+    
+    
 }
