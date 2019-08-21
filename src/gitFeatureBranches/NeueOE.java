e4342a2a94ecc3801f5dbd3c18275213ce54e747
kretschmer.florian <kretschmer.florian@PC230115.osz.intern>
2019-08-21 14:08:38 +0200

    Signed-off-by: kretschmer.florian <kretschmer.florian@PC230115.osz.intern>

--- src/gitFeatureBranches/NeueOE.java
+++ src/gitFeatureBranches/NeueOE.java
@@ -2,5 +2,34 @@ package gitFeatureBranches;
 
 public class NeueOE
 {
+   public class OE
+   {
+      String Name;
+      int ID;
+      public OE(String name, int iD)
+      {
+   	  super();
+   	  Name = name;
+   	  ID = iD;
+      }
+      public String getName()
+      {
+         return Name;
+      }
+      public void setName(String name)
+      {
+         Name = name;
+      }
+      public int getID()
+      {
+         return ID;
+      }
+      public void setID(int iD)
+      {
+         ID = iD;
+      }
 
+   }
+   
+   
 }
