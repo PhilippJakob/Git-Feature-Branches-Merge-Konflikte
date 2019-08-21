3b41ab3915b23ebfae1ed95ceb94133434c8aab6
kretschmer.florian <kretschmer.florian@PC230115.osz.intern>
2019-08-21 13:47:41 +0200

    Signed-off-by: kretschmer.florian <kretschmer.florian@PC230115.osz.intern>

---
+++ src/gitFeatureBranches/OEController.java
@@ -0,0 +1,18 @@
+package gitFeatureBranches;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class OEController {
+
+    @FXML
+    private ChoiceBox<?> cbUeber;
+
+    @FXML
+    private TextField tfName;
+
+    @FXML
+    private ChoiceBox<?> cbStelle;
+
+}
+
