2e8b4b2466c5e77355ef1120b189d9e5bf95fd13
madrian.patrick <madrian.patrick@PC230101.osz.intern>
2019-08-21 14:41:57 +0200

    Bisheriger Stand:Oberfläche zum anlegen, löschen und zuweisen von Gruppen erstellt

---
+++ src/gitFeatureBranches/Gruppe.java
@@ -0,0 +1,6 @@
+package gitFeatureBranches;
+
+public class Gruppe
+{
+
+}
---
+++ src/gitFeatureBranches/GruppenbearbeitungController.java
@@ -0,0 +1,22 @@
+package gitFeatureBranches;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
+   public class GruppenbearbeitungController {
+
+       @FXML
+       private Button btErstellen;
+
+       @FXML
+       private ChoiceBox<?> cbGruppe;
+
+       @FXML
+       private Button btLöschen;
+
+       @FXML
+       private ChoiceBox<?> cbPerson;
+
+   }
+
+
