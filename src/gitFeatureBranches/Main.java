8772a846aaa3a277a572c546d64e04d841cd6802
kretschmer.florian <kretschmer.florian@PC230115.osz.intern>
2019-08-21 14:33:00 +0200

    Signed-off-by: kretschmer.florian <kretschmer.florian@PC230115.osz.intern>

--- src/gitFeatureBranches/Main.java
+++ src/gitFeatureBranches/Main.java
@@ -1,29 +1,38 @@
 package gitFeatureBranches;
 	
+import java.io.IOException;
+
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			//
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+   
+   private Stage 		bühne;
+   private AnchorPane 	grundPane;
+
+   @Override
+   public void start(Stage pPrimaryStage) throws IOException
+   {
+
+       this.bühne = pPrimaryStage;
+       FXMLLoader lLoader = new FXMLLoader();
+
+       lLoader.setLocation(Main.class.getResource("Main.fxml"));
+       grundPane = lLoader.load();
+
+       Scene lScene = new Scene(grundPane);
+       bühne.setScene(lScene);
+       bühne.show();
+   }
 	
 	public static void main(String[] args) {
-	   HrJakobKonflikt newjacob = new HrJakobKonflikt();
-		newjacob.schwimmen();
+	 
 		launch(args);
 		
 		
