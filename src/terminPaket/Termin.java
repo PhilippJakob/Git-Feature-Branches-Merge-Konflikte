ba17d7c2335f2dac5480e40ed832581ebf82875a
karadeniz.murat-can <karadeniz.murat-can@PC320122.osz.intern>
2019-08-21 14:42:58 +0200

    abc

--- src/terminPaket/Termin.java
+++ src/terminPaket/Termin.java
@@ -1,11 +1,40 @@
 package terminPaket;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+
+import com.mysql.jdbc.Connection;
 
 public class Termin
 {
-   public ArrayList <Termin> auslesenTermine(Integer pIdPerson)
+   private static final Datenbankverbindung dbVerbindung = new Datenbankverbindung();
+   
+   public ArrayList <Termin> auslesenTermine(Connection pConnection, Integer pIdPerson)
    {
-	  return null;
+	  int lIdPerson = pIdPerson;
+	  ArrayList<Termin> = new ArrayList<Termin>();
+	  Statement lBefehl;
+	  ResultSet lErgebnis;
+
+
+	     try
+	     {
+		    lBefehl = pConnection.createStatement();
+		    lErgebnis=lBefehl.executeQuery("SELECT * FROM dbpr_termin.termin WHERE IDPerson = " + lIdPerson);
+		    lErgebnis.first();
+		    lIdPerson=lErgebnis.getInt(1);
+		
+	      }
+
+	     catch (SQLException e)
+	     {
+		   
+		     e.printStackTrace();
+	    
+	     }
+
+	     return Termin;
    }
 }
