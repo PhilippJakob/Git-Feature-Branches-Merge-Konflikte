b04c03af34ab353f6661dfeb93b1cfc6ee7cb42b
madrian.patrick <madrian.patrick@PC230101.osz.intern>
2019-08-21 14:51:30 +0200

    DBVerbindung

---
+++ src/gitFeatureBranches/DBVerbindung.java
@@ -0,0 +1,36 @@
+package gitFeatureBranches;  
+import java.sql.*;
+
+public class DBVerbindung 
+{
+   static Connection connection;
+
+   public boolean verbinden(String pHostrechner, String pDatenbank, String pBenutzer, String pPasswort){
+   	try
+   	{
+   	 Class.forName("com.mysql.jdbc.Driver").newInstance(); 
+   	 String connectionCommand = "jdbc:mysql://"+pHostrechner+":3306/"+pDatenbank+"?user="+pBenutzer+"&password="+pPasswort;
+        connection = DriverManager.getConnection(connectionCommand);
+        return true;
+
+    }
+   	catch (Exception ex)
+   	{				
+   	   System.out.println("Fehler");
+   	   return false;
+    }}
+   
+
+   public static Connection holenConnection()
+   {
+   	return connection;
+   }
+
+   public void setzenConnection(Connection pConnection)
+   {
+   	connection = pConnection;
+   }
+    
+   }
+
+
