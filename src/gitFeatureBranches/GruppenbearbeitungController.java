3f02912c26c0cd933b92aa6bee0d1ff88b1be839
madrian.patrick <madrian.patrick@PC230101.osz.intern>
2019-08-26 10:55:31 +0200

    Absoluter trash

--- src/gitFeatureBranches/GruppenbearbeitungController.java
+++ src/gitFeatureBranches/GruppenbearbeitungController.java
@@ -1,4 +1,7 @@
 package gitFeatureBranches;
+import java.util.ArrayList;
+
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -19,6 +22,9 @@ import javafx.scene.control.Tooltip;
        @FXML
        private ChoiceBox<?> cbGruppe;
       
+ 	  private static DBVerbindung dbVerbindung = new DBVerbindung();
+ 	  private static ArrayList<Gruppe> gruppeAL = new ArrayList<Gruppe>();
+
        @FXML
        public void initialize()
        {
@@ -26,6 +32,68 @@ import javafx.scene.control.Tooltip;
     	  {
     		return;
     	  }
+
+    	  setGruppeAL(Gruppe.auslesenDB(DBVerbindung.holenConnection()));
+        	  cbGruppe.getItems().addAll(Gruppe.getGruppe());
+        	  mGruppelöschen.setText("Person löschen");
+        	  mGruppelöschen.setOnAction(new EventHandler<ActionEvent>() {
+        		 
+    		    @Override
+    		    public void handle(ActionEvent event)
+    		    { 
+    		       Stage bühne = new Stage();	
+    		       FXMLLoader lLoader = new FXMLLoader();
+        		       try
+    			   {
+    		    	  	   lLoader.setLocation(getClass().getResource("PersonenloeschenView.fxml"));
+    		    	  	   grundPane = lLoader.load();
+    		    	  	   Scene lScene = new Scene(grundPane);
+    				       bühne.setScene(lScene);
+    				       bühne.show();     
+    			   }
+    			   catch (IOException e)
+    			   {
+    				  // TODO Automatisch generierter Erfassungsblock
+    				  e.printStackTrace();
+    			   }
+    		    }
+    		 });
+        	  mPersonhinzufügen.setOnAction(new EventHandler<ActionEvent>(){
+        		    @Override
+        		    public void handle(ActionEvent event)
+        		    { 
+        		       Stage bühne = new Stage();	
+        		       FXMLLoader lLoader = new FXMLLoader();
+            		       try
+        			   {
+        		    	  	   lLoader.setLocation(getClass().getResource("PersonenhinzufügenView.fxml"));
+        		    	  	   grundPane = lLoader.load();
+        		    	  	   Scene lScene = new Scene(grundPane);
+        				       bühne.setScene(lScene);
+        				       bühne.show();     
+        			   }
+        			   catch (IOException e)
+        			   {
+        				  // TODO Automatisch generierter Erfassungsblock
+        				  e.printStackTrace();
+        			   }
+        		    }
+        		 });
+        	 
+        	  cbPersonauswahl.setTooltip(new Tooltip("Wähle die Person aus"));
+           }
+
+	  public static ArrayList<Gruppe> getGruppeAL()
+	  {
+	     return gruppeAL;
+	  }
+
+	  public static void setGruppeAL(ArrayList<Gruppe> gruppeAL)
+	  {
+	     GruppenbearbeitungController.gruppeAL = gruppeAL;
+	  }
+
+      
 
 
        
