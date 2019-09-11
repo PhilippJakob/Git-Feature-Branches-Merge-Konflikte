0484b36af99cb746cacf70231b8fb31f166f0d2e
abou_tay.rawad <abou_tay.rawad@PC230121.osz.intern>
2019-09-11 14:23:17 +0200

    Absender und Empfänger hinzugefügt

--- src/gitFeatureBranches/EmailController.java
+++ src/gitFeatureBranches/EmailController.java
@@ -1,38 +1,47 @@
 package gitFeatureBranches;
 
 import javax.mail.MessagingException;
-import javax.mail.internet.AddressException;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class EmailController
-{
-   @FXML
-   private TextField tfEmpfänger;
+public class EmailController {
 
-   @FXML
-   private TextField tfBetreff;
+    @FXML
+    private TextField tfBetreff;
 
-   @FXML
-   private TextField tfAbsender;
+    @FXML
+    private TextArea taText;
 
-   @FXML
-   private Button btVersenden;
-   @FXML
-   private TextArea taText;
-   @FXML
-   public void initialize()
-   {
-	  
-   }
-   public void Versenden()
-   {
+    @FXML
+    private Button btVersenden;
+
+    @FXML
+    private ChoiceBox<String> cbAbsender;
+
+    @FXML
+    private Label lbAbsender;
+
+    @FXML
+    private Label lbEmpfänger;
+
+    @FXML
+    private ChoiceBox<String> cbEmpfänger;
+    @FXML
+    public void initialize()
+    {
+       cbAbsender.getItems().addAll(Person.getPersonen()); 
+       cbEmpfänger.getItems().addAll(Person.getPersonen());
+    }
+    public void Versenden()
+    {
 	  try
 	  {
-		 SendEmail.generateAndSendEmail(tfBetreff.getText(), tfAbsender.getText(), tfEmpfänger.getText(), taText.getText());
+		 SendEmail.generateAndSendEmail(tfBetreff.getText(), cbAbsender.getId(),cbEmpfänger.getId(), taText.getText());
 	  }
 	  catch (MessagingException e)
 	  {
@@ -40,5 +49,5 @@ public class EmailController
 	  e.printStackTrace();
 	  }
 
-   }
+    }
 }
