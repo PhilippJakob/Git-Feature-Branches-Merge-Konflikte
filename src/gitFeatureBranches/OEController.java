a359a755f7757e3846fcb546adfc976530fff7df
kretschmer.florian <kretschmer.florian@PC230115.osz.intern>
2019-09-11 12:16:04 +0200

    1

--- src/gitFeatureBranches/OEController.java
+++ src/gitFeatureBranches/OEController.java
@@ -1,76 +1,62 @@
 package gitFeatureBranches;
-import java.io.IOException;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
+
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Tooltip;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import jfxtras.scene.control.agenda.Agenda;
-import jfxtras.scene.control.agenda.AgendaSkinSwitcher;
-public class OEController 
+
+public class OEController
 {
    private static DBVerbindung dbVerbindung = new DBVerbindung();
-   private static ArrayList<Organisationseinheit> organisationseinheitAL = new ArrayList<Organisationseinheit>();
-    @FXML
-    private ChoiceBox<?> cbUeber;
 
-    @FXML
-    private TextField tfName;
+   private int hinzugefügteOE =1 ;
 
-    @FXML
-    private ChoiceBox<?> cbStelle;
+   @FXML
+   private Button btZuweisen;
 
-    @FXML
-    private Button btZuweisen;
-    private Stage 		bühneOrganisationseinheitenhinzufuegen = new Stage();	
-    private AnchorPane 	grundPane;
-    private AnchorPane 	grundPane2;
-   private int hinzugefügteOrganisationseinheiten;
-    
-    public void initialize()
-    {
+   @FXML
+   private TextField tfName;
+   //Füllt CB und Handelt Knopfdruck
+   @FXML
+   public void initialize()
+   {
 
- 	 btZuweisen.setOnAction(new EventHandler<ActionEvent>() {
- 		@Override
- 	    public void handle(ActionEvent event)
- 	    { 
- 		   hinzufügenOrganisationseinheit(tfName.getText(), dbVerbindung.holenConnection());
- 	    }
-    } );
-    }
-    //Holt sich höchste ID und Fügt Person mit höchsterID+1 hinzu.
-    void hinzufügenOrganisationseinheit(String pOrganisationseinheit, Connection connection)
-    {
- 	  Statement lBefehl;
- 	  String ID = new String();
- 	  String[] tokens;
- 	  
- 	  ID = Organisationseinheit.getLetzteOrganisationseinheit();
- 	  tokens = ID.split(" ");
- 	  ID = tokens[tokens.length-1];
- 	  ID = Integer.toString(Integer.parseInt(ID)+hinzugefügteOrganisationseinheiten);
- 	  hinzugefügteOrganisationseinheiten++;
- 	  try
- 	  {
- 		 lBefehl= connection.createStatement();
- 		 lBefehl.executeUpdate("INSERT INTO organisationseinheit(OEID,OENAME) VALUES('"+ID+"','"+pOrganisationseinheit+"',NULL);");
- 	  }
- 	  catch (SQLException e)
- 	  {
- 		 // TODO Automatisch generierter Erfassungsblock
- 		e.printStackTrace();
- 	  }
-    }
+	 btZuweisen.setOnAction(new EventHandler<ActionEvent>() {
+		@Override
+	    public void handle(ActionEvent event)
+	    { 
+		   hinzufügenOrganisationseinheiten(tfName.getText(), dbVerbindung.holenConnection());
+	    }
+   } );
+   }
+   //Holt sich höchste ID und Fügt Person mit höchsterID+1 hinzu.
+   void hinzufügenOrganisationseinheiten(String pOrganisationseinheit, Connection connection)
+   {
+	  Statement lBefehl;
+	  String ID = new String();
+	  String[] tokens;
+	  
+	  ID = Organisationseinheit.getLetzteOrganisationseinheit();
+	  tokens = ID.split(" ");
+	  ID = tokens[tokens.length-1];
+	  ID = Integer.toString(Integer.parseInt(ID)+hinzugefügteOE);
+	  hinzugefügteOE++;
+	  try
+	  {
+		 lBefehl= connection.createStatement();
+		 lBefehl.executeUpdate("INSERT INTO organisationseinheit(OEID,OENAME) VALUES('"+ID+"','"+pOrganisationseinheit+"',NULL);");
+	  }
+	  catch (SQLException e)
+	  {
+		 // TODO Automatisch generierter Erfassungsblock
+		e.printStackTrace();
+	  }
+   }
 }
-
