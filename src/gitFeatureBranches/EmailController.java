0df0650cc1ce350cb74cd2d5a7232ce25a5963a6
abou_tay.rawad <abou_tay.rawad@PC230121.osz.intern>
2019-09-25 15:01:32 +0200

    OE

--- src/gitFeatureBranches/EmailController.java
+++ src/gitFeatureBranches/EmailController.java
@@ -1,5 +1,9 @@
 package gitFeatureBranches;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import javax.mail.MessagingException;
@@ -16,7 +20,8 @@ public class EmailController {
    
    private static ArrayList<Person> personenOEAL	= new ArrayList<Person>();
    private static ArrayList<Person> personenGAL		= new ArrayList<Person>();
-   
+   static Connection connection;
+
      
     @FXML
     private TextField tfBetreff;
@@ -77,15 +82,87 @@ public class EmailController {
 	  }
 
     }
-    
-    public void ermittelnPersonenausGruppe()
+    public void aufsplitten()
     {
-       
+       String[] tokens;
+ 	  String pPerson = null;
+	  tokens = pPerson.split(" ");
+ 	 Integer.parseInt(tokens[tokens.length-1]);
     }
     
     
+    public static ArrayList<Person> auslesenPersonenAusGruppe(String cbGruppen)
+    {
+       
+       ArrayList<Person> personenausgruppe = new ArrayList<Person>();
+       Statement lBefehl;
+       ResultSet lErgebnis;
+       Person lPerson;
+       try
+	  {
+		 lBefehl 	= connection.createStatement();
+	     lErgebnis = lBefehl.executeQuery("select Name,IDPerson from person where IDPerson = ANY(select IDPerson from gruppenzugehörigkeit where IDGruppe = '"+ cbGruppen + "')");
 
+	     
+	     lErgebnis.first(); 
 
+	     while(!lErgebnis.isAfterLast())   
+	        {
+	    	lPerson = new Person(lErgebnis.getString(1),lErgebnis.getInt(2));
+	    	personenausgruppe.add(lPerson);
+	          lErgebnis.next();
+	        }
+	  }
+	  catch (SQLException e)
+	  {
+		 // TODO Automatisch generierter Erfassungsblock
+		e.printStackTrace();
+	  }             
+       return personenausgruppe;
+       
+    }
+    
+    public static ArrayList<Person> auslesenPersonenAusOE(int cbEinheiten)
+    {
+       ArrayList<Person> personenausOE = new ArrayList<Person>();
+       Statement lBefehl;
+       ResultSet lErgebnis;
+       Person lPerson;
+       
+       try
+ 	  {
+ 		 lBefehl 	= connection.createStatement();
+ 	     lErgebnis = lBefehl.executeQuery("Select Name,IDPerson from person where IDPerson = ANY(select IDPerson from `oe-zugehörigkeit` where StID  = '"+ cbEinheiten + "')");
+
+ 	     
+ 	     lErgebnis.first(); 
+
+ 	     while(!lErgebnis.isAfterLast())   
+ 	        {
+ 	    	lPerson = new Person(lErgebnis.getString(1),lErgebnis.getInt(2));
+ 	    	personenausOE.add(lPerson);
+ 	          lErgebnis.next();
+ 	        }
+ 	    ArrayList<Organisationseinheit> UntergeordneteOE = new ArrayList<Organisationseinheit>();
+ 	    
+ 	    
+ 	    
+ 	       for(Organisationseinheit lOE : UntergeordneteOE)
+ 	       {
+ 	    	  personenausOE.addAll(auslesenPersonenAusOE(lOE.getID()));
+ 	    	  
+ 	       } 	       	     
+ 	    		  
+ 	  }
+ 	  catch (SQLException e)
+ 	  {
+ 		 // TODO Automatisch generierter Erfassungsblock
+ 		e.printStackTrace();
+ 	  }             
+        return personenausOE;        
+        
+     }
+            
    public static ArrayList<Person> getPersonenOEAL()
    {
 	  return personenOEAL;
